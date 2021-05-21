b824adbe164904dfeba36eba787883bb39e395ce
Elizabeth Duskey <elizabeth.duskey@slu.se>
2021-05-21 17:11:22 -0400

    Update gamkrige.R

--- Code/gamkrige.R
+++ Code/gamkrige.R
@@ -1,7 +1,7 @@
 # Code to run standard GAM+OK method
 
 # Created: May 19, 2021
-# Last modified: May 21, 2021
+# Last modified: May 21, 2021 by EPD
 
 # Set working directory
 setwd(paste(mypath, "variogam-main", sep = ""))
